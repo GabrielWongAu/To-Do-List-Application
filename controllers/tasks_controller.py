431836d4b703a7df6fae5ac5bde401a56cf66e44
Gabriel Wong <gabriel.wong.au@gmail.com>
2021-03-26 21:20:56 +1100

    Added DELETE and PUT/PATCH Tasks API endpoints to delete and edit tasks

--- controllers/tasks_controller.py
+++ controllers/tasks_controller.py
@@ -21,8 +21,29 @@ def task_create():
     new_task.description = task_fields["description"]
     new_task.created = datetime.now()
 
-    #add a new shelter to the db
+    #add a new task to the db
     db.session.add(new_task)
     db.session.commit()
 
     return jsonify(task_schema.dump(new_task))
+
+@tasks.route("/<int:id>", methods=["GET"])
+def task_show(id):
+    # SELECT * FROM TASKS WHERE ID = id
+    task = Task.query.get(id)
+    return jsonify(task_schema.dump(task))
+
+@tasks.route("/<int:id>", methods=["DELETE"])
+def task_delete(id):
+    task = Task.query.get(id)
+    db.session.delete(task)
+    db.session.commit()
+    return jsonify(task_schema.dump(task))
+
+@tasks.route("/<int:id>", methods=["PUT","PATCH"])
+def task_update(id):
+    tasks = Task.query.filter_by(id=id)
+    task_fields = task_schema.load(request.json)
+    tasks.update(task_fields)
+    db.session.commit()
+    return jsonify(task_schema.dump(tasks[0]))
