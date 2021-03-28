c6ebdcf3e20ece7461ebadf0be1010151fff782b
Gabriel Wong <gabriel.wong.au@gmail.com>
2021-03-28 17:31:39 +1100

    Updated db_create.py

--- db_create.py
+++ db_create.py
@@ -1,2 +1,8 @@
-from app import db
-db.create_all()
+from main import db
+import click
+from flask.cli import with_appcontext
+
+@click.command(name='create_tables')
+@with_appcontext
+def create_tables():
+    db.create_all()
