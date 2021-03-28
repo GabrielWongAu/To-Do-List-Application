825c14ddf07904fa3de3294574ad4408caef85f4
Gabriel Wong <gabriel.wong.au@gmail.com>
2021-03-28 14:29:31 +1100

    Removed from flask_migrate import Migrate in main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from flask_bcrypt import Bcrypt
 # from flask_jwt_extended import JWTManager
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
-from flask_migrate import Migrate
 
 db = SQLAlchemy()
 mar = Marshmallow()
